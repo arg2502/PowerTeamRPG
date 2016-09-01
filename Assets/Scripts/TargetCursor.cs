c95d8f6f4802f25544f7cc440695bffb96a6d668
ebstiv23 <ebs9787@g.rit.edu>
2016-09-01 11:47:51 -0400

    A small script to make secondary cursors appear different from the primary cursor when multi-targeting.

---
+++ Assets/Scripts/TargetCursor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TargetCursor : MonoBehaviour {
+
+    public List<Sprite> sprites;
+    public bool isSplash;
+
+    public void ChangeSprite()
+    {
+        if (isSplash) { gameObject.GetComponent<SpriteRenderer>().sprite = sprites[1]; }
+        if (!isSplash) { gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0]; }
+    }
+}
