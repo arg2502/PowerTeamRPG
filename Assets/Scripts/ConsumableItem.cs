ee53855f35cbd32c17a38815d0b18b7893be1218
ebstiv23 <ebs9787@g.rit.edu>
2016-09-10 00:38:11 -0400

    Added the script shells for the items and their sub-classes

---
+++ Assets/Scripts/ConsumableItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsumableItem : Item {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void Use()
+    {
+        //this method will use the name of the item in a switch
+        //to determine the appropriate method to call to perform
+        //the item's specific effect
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+    //Attributes
+    public string name;
+    public string description;
+    public int quantity;
+    public int price;
+    public Sprite sprite;
+
+	// Use this for initialization
+	protected void Start () {
+	
+	}
+
+    public void Use()
+    {
+        // items will have there own overrides
+    }
+}
