2b1afe1a0efa028ed472256875f41f93f4a55811
Alec Gerchman <arg2502@rit.edu>
2016-09-23 00:45:33 -0400

    Started Skill Tree. Jethro's list appears on screen.

---
+++ Assets/Scripts/SkillTrees/JethroSkillTree.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JethroSkillTree : SkillTree {
+
+    // all possible skills for Jethro
+    Technique helmsplitter;
+    Technique trinitySlice;
+    Technique riser;
+    Technique mordstreich;
+    Technique arcSlash;
+    
+    
+    
+    // divided into columns
+    List<Technique> col1;
+    List<Technique> col2;
+    List<Technique> col3;
+
+
+	// Use this for initialization
+	void Start () {
+        // create all techniques
+        helmsplitter = new Technique();
+        helmsplitter.Name = "Helmsplitter";
+        helmsplitter.Description = "A powerful sword strike from above. \n Str 75, Crit 03, Acc 90";
+        helmsplitter.Pm = 2;
+
+        trinitySlice = new Technique();
+        trinitySlice.Name = "Trinity Slice";
+        trinitySlice.Description = "";
+        trinitySlice.Pm = 0;
+
+        riser = new Technique();
+        riser.Name = "Riser";
+        riser.Description = "";
+        riser.Pm = 0;
+
+        mordstreich = new Technique();
+        mordstreich.Name = "Mordstreich";
+        mordstreich.Description = "";
+        mordstreich.Pm = 0;
+
+        arcSlash = new Technique();
+        arcSlash.Name = "Arc Slash";
+        arcSlash.Description = "";
+        arcSlash.Pm = 0;
+
+        // max num of col
+        numOfColumn = 3;
+
+        // max num of row
+        numOfRow = 2;
+
+        // set content array
+        content2DArray = new List<List<Technique>>();
+        content2DArray.Add(new List<Technique>(){helmsplitter, trinitySlice});
+        content2DArray.Add(new List<Technique>() { arcSlash });
+        content2DArray.Add(new List<Technique>() { riser, mordstreich });
+
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SkillTrees/SkillTree.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SkillTree : MonoBehaviour {
+
+    // 2D array of button layout
+    // COLUMN MAJOR (i = column number, j = row number)
+    GameObject[,] button2DArray;
+    
+    // button states
+    // inactive = player does not have the skill, but has the possibility to acquire it
+    // disabled = skill is locked off to player/has to unlock an earlier skill to make it inactive
+
+    // List containing all of a column's technique
+    // Example contentArray[0] = {"Fire Breath Lvl 1", "Fire Breath Lvl 2", etc.}
+    protected List<List<Technique>> content2DArray;
+
+    // ints to give the size of the 2D Array
+    public int numOfColumn;
+    public int numOfRow;
+
+    // 2 ints to keep track of where you are in the button2DArray
+    int columnIndex;
+    int rowIndex;
+
+    // access to camera
+    protected GameObject camera;
+
+    public void Start()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        // sizes are set in child classes
+        // content2DArray is also set in child classes
+        // then base.Start() is called
+        button2DArray = new GameObject[numOfColumn, numOfRow];
+
+        // set indexs to start
+        columnIndex = 0;
+        rowIndex = 0;
+
+        // nest for loop through 2D array
+        // whereever there is content, create a button
+        for(int col = 0; col < numOfColumn; col++)
+        {
+            for(int row = 0; row < numOfRow; row++)
+            {
+                // if we've surpassed the length of the content in this column,
+                // break out of loop
+                
+                if(row >= content2DArray[col].Count)
+                {
+                    print("break");
+                    break;
+                }
+                // otherwise, create a button
+                else
+                {
+                    button2DArray[col,row] = (GameObject)Instantiate(Resources.Load("Prefabs/ButtonPrefab"));
+                    MyButton b = button2DArray[col,row].GetComponent<MyButton>();
+                    button2DArray[col,row].transform.position = new Vector2(camera.transform.position.x - 600 + (col * (b.width + b.width/2)), camera.transform.position.y - (250 - b.height) + (row * -(b.height + b.height / 2)));
+
+                    // display technique's name
+                    b.textObject = (GameObject)Instantiate(Resources.Load("Prefabs/CenterTextPrefab"));
+                    b.labelMesh = b.textObject.GetComponent<TextMesh>();
+                    b.labelMesh.text = content2DArray[col][row].Name;
+                    b.labelMesh.transform.position = new Vector3(button2DArray[col,row].transform.position.x, button2DArray[col,row].transform.position.y, -1);
+                }
+                print(row);
+            }
+        }
+
+
+    }
+
+    public void Update()
+    {
+        
+    }
+}
