76b001e366365074d502a2b824081fe5a7875232
ebstiv23 <ebs9787@g.rit.edu>
2016-10-21 18:37:21 -0400

    All currently existing armor can also be equipped to heroes through the Inventory Menu.

--- Assets/Scripts/ItemScripts/ArmorItem.cs
+++ Assets/Scripts/ItemScripts/ArmorItem.cs
@@ -20,18 +20,84 @@ public class ArmorItem : Item {
         //this method will use the name of the item in a switch
         //to determine the appropriate method to call to perform
         //the item's specific effect
+        switch (name)
+        {
+            case "Helmet of Fortitude":
+                hero.def += defChange;
+                hero.luck += luckChange;
+                break;
+            case "Iron Helm":
+                hero.def += defChange;
+                break;
+            case "Steel Helm":
+                hero.def += defChange;
+                break;
+            case "Iron Armor":
+                hero.def += defChange;
+                break;
+            case "Steel Armor":
+                hero.def += defChange;
+                break;
+            case "Steel Gauntlets":
+                hero.def += defChange;
+                break;
+            default:
+                print(name + " does not have a case!");
+                break;
+        }
+
+        // find the appropriate object to replace
+        if (hero.equipment.Count > 0)
+        {
+            for (int i = 0; i < hero.equipment.Count; i++)
+            {
+                if (hero.equipment[i].GetComponent<ArmorItem>().type == type)
+                {
+                    hero.equipment[i].GetComponent<ArmorItem>().Remove(hero);
+                    hero.equipment[i] = this.gameObject;
+                }
+            }
+        }
+        else { hero.equipment.Add(this.gameObject); }
+        
+        // Always decrease quantity by 1 to avoid multiple heroes sharing the same piece of armor
+        quantity--;
+        // if quantity hits 0, remove it from the inventory
+        if (quantity <= 0) { GameControl.control.equipment.Remove(this.gameObject); }
     }
 
-    public void Equip()
+    public void Remove(HeroData hero)
     {
-        // functions similarly to the use method, but this method will add
-        // passives or boost stats when called. It may require passing in
-        // the denigen who is equipping the item as an argument
-    }
+        // Does the equip function in reverse, so it subtracts instead of adding
+        switch (name)
+        {
+            case "Helmet of Fortitude":
+                hero.def -= defChange;
+                hero.luck -= luckChange;
+                break;
+            case "Iron Helm":
+                hero.def -= defChange;
+                break;
+            case "Steel Helm":
+                hero.def -= defChange;
+                break;
+            case "Iron Armor":
+                hero.def -= defChange;
+                break;
+            case "Steel Armor":
+                hero.def -= defChange;
+                break;
+            case "Steel Gauntlets":
+                hero.def -= defChange;
+                break;
+            default:
+                print(name + " does not have a case!");
+                break;
+        }
 
-    public void Remove()
-    {
-        // Does the equip function in reverse, so it subtracts instead of
-        // adding, still may require arg
+        // make it possible to use the item again
+        quantity++;
+        // if this item is not in the inventory anymore, add it back in
+        if (quantity == 1) { GameControl.control.equipment.Add(this.gameObject); }
     }
 }
