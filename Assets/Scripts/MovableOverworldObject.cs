86cabb6aa3227f76c4a209df3abd4f07d764fb75
ebstiv23 <ebs9787@g.rit.edu>
2016-10-26 16:28:10 -0400

    Switch started. Currently not operational.

--- Assets/Scripts/MovableOverworldObject.cs
+++ Assets/Scripts/MovableOverworldObject.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class MovableOverworldObject : OverworldObject {
 
     public float collisionOffset;
+    public bool isActivated;
+
+    void Start()
+    {
+        base.Start();
+        if (!isActivated) { gameObject.SetActive(false); }
+    }
+
     //check collisions
     public bool CheckCollisions(Vector3 dir)
     {
@@ -16,16 +24,6 @@ public class MovableOverworldObject : OverworldObject {
         RaycastHit2D hit7 = Physics2D.Raycast(new Vector3(transform.position.x + collisionOffset, transform.position.y - collisionOffset - 8.0f, transform.position.z), dir, 8.0f, mask);
         RaycastHit2D hit8 = Physics2D.Raycast(new Vector3(transform.position.x - collisionOffset, transform.position.y + collisionOffset, transform.position.z), dir, 8.0f, mask);
 
-        //Debug.DrawRay(new Vector3(transform.position.x + collisionOffset, transform.position.y, transform.position.z), dir, Color.red);
-        //Debug.DrawRay(new Vector3(transform.position.x - collisionOffset, transform.position.y, transform.position.z), dir, Color.red);
-        //Debug.DrawRay(new Vector3(transform.position.x, transform.position.y - collisionOffset - 8.0f, transform.position.z), dir, Color.red);
-        //Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + collisionOffset, transform.position.z), dir, Color.red);
-
-        //Debug.DrawRay(new Vector3(transform.position.x + collisionOffset, transform.position.y + collisionOffset, transform.position.z), dir, Color.white);
-        //Debug.DrawRay(new Vector3(transform.position.x - collisionOffset, transform.position.y - collisionOffset, transform.position.z), dir, Color.white);
-        //Debug.DrawRay(new Vector3(transform.position.x + collisionOffset, transform.position.y - collisionOffset, transform.position.z), dir, Color.white);
-        //Debug.DrawRay(new Vector3(transform.position.x - collisionOffset, transform.position.y + collisionOffset, transform.position.z), dir, Color.white);
-
         if ((hit1.collider != null && hit1.collider != this.GetComponent<Collider2D>()) || (hit2.collider != null && hit2.collider != this.GetComponent<Collider2D>())
             || (hit3.collider != null && hit3.collider != this.GetComponent<Collider2D>()) || (hit4.collider != null && hit4.collider != this.GetComponent<Collider2D>())) { return true; }
         else if((hit5.collider != null && hit5.collider != this.GetComponent<Collider2D>()) || (hit6.collider != null && hit6.collider != this.GetComponent<Collider2D>())
@@ -34,6 +32,10 @@ public class MovableOverworldObject : OverworldObject {
     }
 	// Update is called once per frame
 	void Update () {
-        sr.sortingOrder = (int)-transform.position.y;
+        if (isActivated)
+        {
+            gameObject.SetActive(true);
+            sr.sortingOrder = (int)-transform.position.y;
+        }
 	}
 }
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Switch : OverworldObject {
+
+    public enum SwitchType { activateObj, createObj, openDoor };
+    // activate object affects a non-door obj in the room
+    // create object does what it's name implies
+    // open door opens an already existing door
+
+    public SwitchType switchType; // determines what the switch will do once activated
+
+    public bool isActivated = false; // whether or not the player touched the switch
+
+    public List<GameObject> affectedObjs; // stores a list of objects to be effected by the switch
+    // blocks will have an active bool, so they always exist, but the switch activates them
+    //public List<Vector2> objPositions; // stores the positions of newly created objects, such as blocks
+    
+    float distFromPlayer;
+    Transform player;
+
+	// Use this for initialization
+	void Start () {
+        base.Start();
+        player = GameObject.FindObjectOfType<characterControl>().transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Space) && !isActivated)
+        {
+            distFromPlayer = Mathf.Abs(Mathf.Sqrt(((transform.position.x - player.position.x) * (transform.position.x - player.position.x))
+            + ((transform.position.y - player.position.y) * (transform.position.y - player.position.y))));
+
+            if (distFromPlayer < 100.0f)
+            {
+                isActivated = true;
+                sr.color = Color.green; // would change the sprite, but for now just change color
+
+                // perform the appropriate activation
+                if (switchType == SwitchType.createObj) 
+                { 
+                    foreach (GameObject go in affectedObjs) 
+                    {
+                        if (go.GetComponent<MovableOverworldObject>() != null) { go.GetComponent<MovableOverworldObject>().isActivated = true; }
+                    }
+                }
+            }
+        }
+	}
+}
