063b6ecb5f0bb7b5bb5496b1f76badb36b37245f
Alec Gerchman <arg2502@rit.edu>
2016-11-01 01:09:33 -0400

    Started level design level. Also edited hole distance. Added collider list to drawbridge. Created Hole Locked Door.

--- Assets/Scripts/Drawbridge.cs
+++ Assets/Scripts/Drawbridge.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Drawbridge : OverworldObject {
 
     int height = 500;
     float initialPos;
+    public List<GameObject> colliders;
 
     void Start()
     {
@@ -18,11 +20,19 @@ public class Drawbridge : OverworldObject {
     }
     public IEnumerator LowerBridge()
     {
+        // if the bridge is not at it's designated location, keep moving it
         while (transform.position.y > initialPos - height)
         {
-            transform.Translate(new Vector3(0.0f, -1.0f, 0.0f));
+            transform.Translate(new Vector3(0.0f, -2.0f, 0.0f));
             yield return new WaitForSeconds(0.01f);
 
         }
+        // when bridge is in location, deactivate colliders
+        foreach(GameObject go in colliders)
+        {
+            go.SetActive(false);
+        }
+
+
     }
 }
