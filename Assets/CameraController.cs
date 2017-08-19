e87207ae210616208bf1032a7edc72413b5e3d88
Alec Gerchman <arg2502@rit.edu>
2017-08-19 01:17:01 -0400

    Changed camera so instead of childing the player, it has a script that follows it. This could allow for a more dynamic camera. Even possibly lagging behind the player or not following the player at all (suitable for smaller rooms)

---
+++ Assets/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject followTarget;
+    Vector3 targetPos;
+    public float moveSpeed;
+
+	// Update is called once per frame
+	void Update () {
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);	
+	}
+}
