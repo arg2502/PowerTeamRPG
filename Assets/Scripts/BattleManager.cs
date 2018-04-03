bbd9761f7998e87ba1cb6a615136919f04b9f265
Alec Gerchman <arg2502@rit.edu>
2018-04-02 22:56:17 -0400

    Fixes issue where fleeing after selecting one hero's attacks was skipping the first enemy's attacks

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -696,7 +696,7 @@ public class BattleManager : MonoBehaviour {
             hero.CurrentAttackName = "";
 
         yield return new WaitForSeconds(1f);
-        StartAttackPhase();
+        GoToAttackState();
     }
 
     // STATS CARDS
