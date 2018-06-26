facc8806dcbc9f8041b0c4e1d7b592f7943980f4
Alec Gerchman <arg2502@rit.edu>
2018-06-25 21:14:26 -0400

    Added (super) basic AudioManager to play placeholder battle music

---
+++ Assets/Scripts/GameControl/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    // SUPER BASIC FOR NOW -- REFORMAT LATER
+
+    public AudioClip battleIntro;
+    public AudioClip battleLoop;
+    AudioSource mus_Source;
+
+	// Use this for initialization
+	void Start () {
+        mus_Source = GetComponent<AudioSource>();
+        StartCoroutine(StartThenLoop());		
+	}
+	
+    IEnumerator StartThenLoop()
+    {
+        mus_Source.clip = battleIntro;
+        mus_Source.loop = false;
+        mus_Source.Play();
+
+        yield return new WaitUntil(() => mus_Source.time >= battleIntro.length);
+
+        mus_Source.clip = battleLoop;
+        mus_Source.loop = true;
+        mus_Source.Play();
+    }
+}
