d629c491bff04a40676d357ee9ba34da1278e443
Alec Gerchman <arg2502@rit.edu>
2018-11-06 01:10:08 -0500

    Set up basic version of dialogue system that will be used. - A conversation is read in through a spreadsheet (converted to .tsv and then changed to .txt so Unity can support it as a TextAsset) - The file is then assigned to an object, currently through the TEST_NPC class (which is temporary until it is combined with a real NPC class)

---
+++ Assets/Scripts/NPCs/Dialogue.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+    TEST_NPC speaker;
+    string currentSpeakerName;
+    Sprite currentSpeakerSprite;
+    string currentDialogueText;
+
+    List<string> speakerNames;
+    List<Sprite> speakerEmotions;
+    List<string> dialogueConversation;
+
+    int conversationIterator = 0;
+
+    private void Start()
+    {
+        speaker = GetComponent<TEST_NPC>();
+    }
+
+    public void StartDialogue(TextAsset textAsset)
+    {
+        DecipherConversation(textAsset);
+        PrintConversation();
+    }
+
+    /// <summary>
+    /// Fill out the lists for the conversation based on the passed in file
+    /// </summary>
+    /// <param name="textAsset"></param>
+    void DecipherConversation(TextAsset textAsset)
+    {
+        speakerNames = new List<string>();
+        speakerEmotions = new List<Sprite>();
+        dialogueConversation = new List<string>();
+
+        // split whole doc into row
+        // each row makes up a sentence or section of the dialogue
+        // could be possible to have someone different speak on each row
+        var rows = textAsset.text.Split('\n');
+        
+        // Loop through the lines (skipping the first line, as that contains var names like "name", "emotion", etc.
+        // Add the appropriate variables to the lists
+        for(int i = 1; i < rows.Length; i++)
+        {
+            // split rows further by dividing them by 'tab' space, since TextAsset was originally a .tsv
+            var lines = rows[i].Split('\t');
+
+            // the first column should be the name of the speaker
+            // if blank, use the NPC's name
+            if (string.IsNullOrEmpty(lines[0]))
+                speakerNames.Add(speaker.npcName);
+            else
+                speakerNames.Add(lines[0]);
+
+            // Next, we had emotional response.
+            // TODO: come up with a way to know which character's sprite to add.
+            // FOR NOW: just add the NPC's image
+            if (string.IsNullOrEmpty(lines[1]))
+                speakerEmotions.Add(speaker.neutralSpr);
+            else
+            {
+                switch (lines[1])
+                {
+                    case "HAPPY":
+                        speakerEmotions.Add(speaker.happySpr);
+                        break;
+                    case "NEUTRAL":
+                        speakerEmotions.Add(speaker.neutralSpr);
+                        break;
+                }
+            }
+
+            // Finally, adding the dialogue. This should never be null so no need to check
+            dialogueConversation.Add(lines[2]);
+        }
+    }
+
+    void PrintConversation()
+    {
+        currentSpeakerName = speakerNames[conversationIterator];
+        currentSpeakerSprite = speakerEmotions[conversationIterator];
+        currentDialogueText = dialogueConversation[conversationIterator];
+
+        print(currentSpeakerName);
+        print(currentSpeakerSprite);
+        print(currentDialogueText);
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(conversationIterator < dialogueConversation.Count - 1)
+            {
+                conversationIterator++;
+                PrintConversation();
+            }
+            else
+            {
+                print("END");
+                conversationIterator = 0;
+                speaker.EndDialogue();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NPCs/TEST_NPC.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_NPC : MonoBehaviour {
+    
+    // each item in the list is a dialogue exchange you have with an NPC or object
+    // if they only say one thing each time you talk, the list will have a count of 1.
+    public List<TextAsset> dialogueList;
+
+    int numOfTimesTalked;
+
+    public string npcName;
+    public Sprite neutralSpr;
+    public Sprite happySpr;
+
+    Dialogue dialogue;
+    bool isTalking = false;
+
+    // Use this for initialization
+    void Start () {
+        dialogue = GetComponent<Dialogue>();
+	}
+
+    /// <summary>
+    /// Send the appropriate conversation over to the Dialogue class to handle
+    /// </summary>
+    void StartDialogue()
+    {
+        dialogue.StartDialogue(dialogueList[numOfTimesTalked]);
+        isTalking = true;
+    }
+
+    public void EndDialogue()
+    {
+        if (numOfTimesTalked < dialogueList.Count - 1)
+            numOfTimesTalked++;
+        isTalking = false;
+    }
+
+    private void Update()
+    {
+        if(!isTalking && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartDialogue();
+        }
+    }
+}
