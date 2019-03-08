0b164c322aecc653a159d8c9277055c8ad160565
Alec Gerchman <arg2502@rit.edu>
2019-03-08 13:05:27 -0500

    Added proper SFX back into battle

--- Assets/Scripts/PlaySound.cs
+++ Assets/Scripts/PlaySound.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class gets attached to sprites with animations 
+/// that need to play a sound on a certain frame
+/// </summary>
 public class PlaySound : MonoBehaviour {
 
 	public void Play(AudioClip clip)
     {
-        //AudioManager.instance.Play(clip);
+        GameControl.audioManager.PlaySFX(clip);
     }
 }
