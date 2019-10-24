1a3ce940d8fde7e4d4d1111dcc0f702ed7f989d0
ebstiv23 <ebs9787@g.rit.edu>
2019-10-23 23:25:47 -0400

    Hidden room functionality added to testCatacomb scene. Switched the room revealer to work with coroutines instead of update method, which seems to have fixed the strange transition effect that happened when running back and forth between rooms.

---
+++ Assets/Scripts/HiddenRoomOverlay.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenRoomOverlay : MonoBehaviour {
+
+	public SpriteRenderer sr; // stores obj's sprite renderer
+	bool isRevealed = false;
+	float fadeTime = 0.0f;
+	Color tmp; // stores temporary color to allow alteration of sr.color
+	// Use this for initialization
+	void Start () {
+		sr = gameObject.GetComponent<SpriteRenderer>();
+		tmp = sr.color;
+		if (isRevealed) {
+			tmp.a = 0f;
+		} else {
+			tmp.a = 1f;
+		}
+		sr.color = tmp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void StartFading(bool _isReveal, float _speed){
+		isRevealed = _isReveal;
+		StopFading ();
+		if (isRevealed && sr.color.a != 0f) {
+			fading = StartCoroutine (Fade (sr.color.a, 0f, _speed));
+		} else if (!isRevealed && sr.color.a != 1.0f) {
+			fading = StartCoroutine (Fade (sr.color.a, 1f, _speed));
+		}
+	}
+
+	public Coroutine fading; // for referencing active coroutines
+	bool isFading{ get { return fading != null; } } // return whether or not a coroutine is active
+
+	public IEnumerator Fade(float _oldColor, float _newColor, float _speed){
+
+		//fadeTime = _oldColor;
+
+		while (fadeTime <= 1.0f) {
+			fadeTime += Time.deltaTime * _speed;
+			
+			tmp = sr.color;
+			tmp.a = Mathf.Lerp (_oldColor, _newColor, fadeTime);
+
+			sr.color = tmp;
+			yield return null;
+		}
+
+		StopFading ();
+	}
+
+	public void StopFading(){
+		if (isFading) {
+			StopCoroutine(fading);
+			fading = null;
+			fadeTime = 0f;
+		}
+	}
+}
---
+++ Assets/Scripts/HiddenRoomTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenRoomTrigger : MonoBehaviour {
+
+	public float transitionSpeed = 1.0f;
+	public List<HiddenRoomOverlay> listOfOverlays;
+	public bool isExit = false;
+
+	void OnTriggerExit2D(Collider2D _other){
+		if (_other.tag == "Player") {
+			if(isExit){
+				foreach( var overlay in listOfOverlays)
+				{
+					//overlay.isRevealed = false;
+					overlay.StartFading(false, transitionSpeed);
+				}
+			}
+			else
+			{
+				foreach( var overlay in listOfOverlays)
+				{
+					//overlay.isRevealed = true;
+					overlay.StartFading(true, transitionSpeed);
+				}
+			}
+		}
+	}
+}
